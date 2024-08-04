b47db1037b69499c15b0fab575b8f07cec418025
chuljex <embesiu@gmail.com>
2024-08-04 22:20:50 +0700

    new update

--- Intro/intro.js
+++ Intro/intro.js
@@ -1,12 +1,12 @@
 var tik = setInterval(() => {
-  var lyf = document.getElementById('digit')
+  var lyf = document.getElementById("digit");
   lyf.classList.toggle("digit");
   if (lyf.innerText == 0) {
-    document.getElementById('container').classList.toggle("containeroverlay")
-    window.location="./MyBirthday/MyBirthday.html";
-    clearInterval(tik)
+    document.getElementById("container").classList.toggle("containeroverlay");
+    window.location = "./Birthday/Birthday.html";
+    clearInterval(tik);
   } else {
-    lyf.innerText -= 1
+    lyf.innerText -= 1;
   }
   lyf.classList.toggle("digit");
-}, 1000)
+}, 1000);
